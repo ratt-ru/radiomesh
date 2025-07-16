1971f70442448e7e49ada4cf9b0d98cc3063c2bd
landmanbester <lbester@sarao.ac.za>
2025-07-16 10:56:32 +0200

    merge in remove-es-kernel-0.5-offset

--- radiomesh/core.py
+++ radiomesh/core.py
@@ -1,38 +1,19 @@
 from functools import partial
+
 import numpy as np
-import sympy as sm
-from numba import literally, njit, types
-from numba.extending import overload
-from sympy.physics.quantum import TensorProduct
-from sympy.utilities.lambdify import lambdify
 from ducc0.fft import good_size
-from numpy.fft import fft2, ifft2
+from numba import literally, njit
+from numba.extending import overload
+from numpy.fft import ifft2
+
+from radiomesh.constants import LIGHTSPEED
+from radiomesh.stokes import stokes_funcs
+from radiomesh.utils import wgridder_conventions
+
 Fs = partial(np.fft.fftshift, axes=(-2, -1))
 iFs = partial(np.fft.ifftshift, axes=(-2, -1))
 
-from radiomesh.constants import LIGHTSPEED
-from radiomesh.utils import wgridder_conventions
-
-JIT_OPTIONS = {
-    "nogil": True,
-    "cache": True,
-    "error_model": 'numpy',
-    "fastmath": False
-}
-
-def wgridder_conventions(l0, m0):
-    '''
-    Returns
-
-        flip_u, flip_v, flip_w, x0, y0
-
-    according to the conventions documented here
-     
-        https://github.com/mreineck/ducc/issues/34
-
-    These conventions are chosen to match the wgridder in ducc
-    '''
-    return False, True, False, -l0, -m0
+JIT_OPTIONS = {"nogil": True, "cache": True, "error_model": "numpy", "fastmath": False}
 
 
 # hardcode 2D gridding params
@@ -40,28 +21,30 @@ def wgridder_conventions(l0, m0):
 # 8, 1.40, 3.1679034e-05, 1.83155364990234371, 0.516968027750650871  (single precision)
 # 8, 1.40, 8.5117152e-06, 1.82943505181206612, 0.517185719807942368  (double precision)
 
-def grid_corrector(ng, supp, beta, alpha, nroots=32):
-    # even number of roots required to exploit even symmetry of integrand
-    if nroots%2:
-        nroots += 1
-    dom = np.linspace(-0.5, 0.5, ng, endpoint=False)
-    q, wgt = np.polynomial.legendre.leggauss(nroots)
-    idx = q > 0
-    q = q[idx]
-    wgt = wgt[idx]
-    z = np.outer(dom, q)
-    xkern = np.zeros(q.size)
-    _es_kernel(q, xkern, supp*beta, alpha)
-    tmp = np.sum(np.cos(np.pi*supp*z) * xkern[None, :] * wgt[None, :], axis=-1)
-    return supp*tmp
 
-@njit(**JIT_OPTIONS, inline='always')
+def grid_corrector(ng, supp, beta, alpha, nroots=32):
+  # even number of roots required to exploit even symmetry of integrand
+  if nroots % 2:
+    nroots += 1
+  dom = np.linspace(-0.5, 0.5, ng, endpoint=False)
+  q, wgt = np.polynomial.legendre.leggauss(nroots)
+  idx = q > 0
+  q = q[idx]
+  wgt = wgt[idx]
+  z = np.outer(dom, q)
+  xkern = np.zeros(q.size)
+  _es_kernel(q, xkern, supp * beta, alpha)
+  tmp = np.sum(np.cos(np.pi * supp * z) * xkern[None, :] * wgt[None, :], axis=-1)
+  return supp * tmp
+
+
+@njit(**JIT_OPTIONS, inline="always")
 def _es_kernel(x, kern, betak, alphak):
-    oneminxsq = (1 - x) * (1 + x)
-    mx = oneminxsq <= 1
-    kern[mx] = np.exp(betak*(pow(oneminxsq[mx], alphak) - 1))
-    kern[~mx] = 0.0
-    return kern
+  oneminxsq = (1 - x) * (1 + x)
+  mx = oneminxsq <= 1
+  kern[mx] = np.exp(betak * (pow(oneminxsq[mx], alphak) - 1))
+  kern[~mx] = 0.0
+  return kern
 
 
 @njit(**JIT_OPTIONS)
@@ -123,190 +106,142 @@ def _grid_data_impl(
   raise NotImplementedError
 
 
-@overload(_grid_data_impl, prefer_literal=True,
-          jit_options={**JIT_OPTIONS, "parallel":True})
-def nb_grid_data_impl(data, weight, flag, jones, tbin_idx, tbin_counts,
-                      ant1, ant2, nx, ny, pol, product, nc,
-                      padding=2.0, supp=8, beta=2.3, alpha=0.5):
+@overload(
+  _grid_data_impl, prefer_literal=True, jit_options={**JIT_OPTIONS, "parallel": True}
+)
+def nb_grid_data_impl(
+  data,
+  weight,
+  flag,
+  jones,
+  tbin_idx,
+  tbin_counts,
+  ant1,
+  ant2,
+  nx,
+  ny,
+  pol,
+  product,
+  nc,
+  padding=2.0,
+  supp=8,
+  beta=2.3,
+  alpha=0.5,
+):
+  vis_func, wgt_func = stokes_funcs(data, jones, product, pol, nc)
+  ns = len(product.literal_value)
+  flip_u, flip_v, flip_w, _, _ = wgridder_conventions(0.0, 0.0)
+  usign = -1.0 if flip_u else 1.0
+  vsign = -1.0 if flip_v else 1.0
 
-    vis_func, wgt_func = stokes_funcs(data, jones, product, pol, nc)
-    ns = len(product.literal_value)    
-    flip_u, flip_v, flip_w, x0, y0 = wgridder_conventions(x0, y0)
-    usign = -1.0 if flip_u else 1.0
-    vsign = -1.0 if flip_v else 1.0
-    
-    ngx = good_size(int(padding*nx))
-    # make sure it is even and a good size for the FFT
-    while ngx%2:
-        ngx = good_size(ngx+1)
-    xcorrector = grid_corrector(ngx, supp, beta, alpha)
-    padxl = (ngx-nx)//2
-    padxr = ngx-nx-padxl
-    slcx = slice(padxl, padxr)
-    ngy = good_size(int(padding*ny))
-    # make sure it is even and a good size for the FFT
-    while ngy%2:
-        ngy = good_size(ngy+1)
-    ycorrector = grid_corrector(ngy, supp, beta, alpha)
-    padyl = (ngy-ny)//2
-    padyr = ngy-ny-padyl
-    slcy = slice(padyl, padyr)
+  ngx = good_size(int(padding * nx))
+  # make sure it is even and a good size for the FFT
+  while ngx % 2:
+    ngx = good_size(ngx + 1)
+  xcorrector = grid_corrector(ngx, supp, beta, alpha)
+  padxl = (ngx - nx) // 2
+  padxr = ngx - nx - padxl
+  slcx = slice(padxl, padxr)
+  ngy = good_size(int(padding * ny))
+  # make sure it is even and a good size for the FFT
+  while ngy % 2:
+    ngy = good_size(ngy + 1)
+  ycorrector = grid_corrector(ngy, supp, beta, alpha)
+  padyl = (ngy - ny) // 2
+  padyr = ngy - ny - padyl
+  slcy = slice(padyl, padyr)
 
-    def _impl(data, weight, flag, uvw, freq, jones, tbin_idx, tbin_counts,
-              ant1, ant2, ngx, ngy, cell_size_x, cell_size_y,
-              supp, beta, alpha):
-        # for dask arrays we need to adjust the chunks to
-        # start counting from zero
-        tbin_idx -= tbin_idx.min()
-        nt = np.shape(tbin_idx)[0]
-        nrow, nchan, ncorr = data.shape
-        vis_grid = np.zeros((ns, ngx, ngy), dtype=data.dtype)
-        # wgt_grid = np.zeros((ns, ngx, ngy), dtype=data.real.dtype)
+  def _impl(
+    data,
+    weight,
+    flag,
+    uvw,
+    freq,
+    jones,
+    tbin_idx,
+    tbin_counts,
+    ant1,
+    ant2,
+    ngx,
+    ngy,
+    cell_size_x,
+    cell_size_y,
+    supp,
+    beta,
+    alpha,
+  ):
+    # for dask arrays we need to adjust the chunks to
+    # start counting from zero
+    tbin_idx -= tbin_idx.min()
+    nt = np.shape(tbin_idx)[0]
+    nrow, nchan, ncorr = data.shape
+    vis_grid = np.zeros((ns, ngx, ngy), dtype=data.dtype)
+    # wgt_grid = np.zeros((ns, ngx, ngy), dtype=data.real.dtype)
 
-        # ufreq
-        u_cell = 1/(nx*cell_size_x)
-        umax = 1/cell_size_x/2
+    # ufreq
+    u_cell = 1 / (nx * cell_size_x)
+    umax = 1 / cell_size_x / 2
 
-        # vfreq
-        v_cell = 1/(ny*cell_size_y)
-        vmax = 1/cell_size_y/2
+    # vfreq
+    v_cell = 1 / (ny * cell_size_y)
+    vmax = 1 / cell_size_y / 2
 
-        normfreq = freq / LIGHTSPEED
-        ko2 = supp/2
-        betak = beta*supp
-        pos = np.arange(supp) - ko2
-        xkern = np.zeros(supp)
-        ykern = np.zeros(supp)
-        for t in range(nt):
-            for row in range(tbin_idx[t],
-                             tbin_idx[t] + tbin_counts[t]):
-                p = int(ant1[row])
-                q = int(ant2[row])
-                gp = jones[t, p, :, 0]
-                gq = jones[t, q, :, 0]
-                uvw_row = uvw[row]
-                wgt_row = weight[row]
-                vis_row = data[row]
-                for chan in range(nchan):
-                    if flag[row, chan]:
-                        continue
-                    wgt = wgt_func(gp[chan], gq[chan],
-                                   wgt_row[chan])
-                    vis = vis_func(gp[chan], gq[chan],
-                                   wgt_row[chan],
-                                   vis_row[chan])
+    normfreq = freq / LIGHTSPEED
+    ko2 = supp / 2
+    betak = beta * supp
+    pos = np.arange(supp) - ko2
+    xkern = np.zeros(supp)
+    ykern = np.zeros(supp)
+    for t in range(nt):
+      for row in range(tbin_idx[t], tbin_idx[t] + tbin_counts[t]):
+        p = int(ant1[row])
+        q = int(ant2[row])
+        gp = jones[t, p, :, 0]
+        gq = jones[t, q, :, 0]
+        uvw_row = uvw[row]
+        wgt_row = weight[row]
+        vis_row = data[row]
+        for chan in range(nchan):
+          if flag[row, chan]:
+            continue
+          wgt = wgt_func(gp[chan], gq[chan], wgt_row[chan])
+          vis = vis_func(gp[chan], gq[chan], wgt_row[chan], vis_row[chan])
 
-          # current uv coords
-          chan_normfreq = normfreq[chan]
-          u_tmp = uvw_row[0] * chan_normfreq * usign
-          v_tmp = uvw_row[1] * chan_normfreq * vsign
-          # pixel coordinates
-          ug = (u_tmp + umax) / u_cell
-          vg = (v_tmp + vmax) / v_cell
-          # indices
-          u_idx = int(np.round(ug))
-          v_idx = int(np.round(vg))
+        # current uv coords
+        chan_normfreq = normfreq[chan]
+        u_tmp = uvw_row[0] * chan_normfreq * usign
+        v_tmp = uvw_row[1] * chan_normfreq * vsign
+        # pixel coordinates
+        ug = (u_tmp + umax) / u_cell
+        vg = (v_tmp + vmax) / v_cell
+        # indices
+        u_idx = int(np.round(ug))
+        v_idx = int(np.round(vg))
 
-          # the kernel is separable and only defined on [-1,1]
-          # do we ever need to check these bounds?
-          x_idx = pos + u_idx
-          x = (x_idx - ug)/ko2
-          _es_kernel(x, xkern, betak, alpha)
-          y_idx = pos + v_idx
-          y = (y_idx - vg)/ko2
-          _es_kernel(y, ykern, betak, alpha)
+        # the kernel is separable and only defined on [-1,1]
+        # do we ever need to check these bounds?
+        x_idx = pos + u_idx
+        x = (x_idx - ug) / ko2
+        _es_kernel(x, xkern, betak, alpha)
+        y_idx = pos + v_idx
+        y = (y_idx - vg) / ko2
+        _es_kernel(y, ykern, betak, alpha)
 
-          for c in range(ncorr):
-              wc = wgt[c]
-              for i, xi in zip(x_idx, xkern):
-                  for j, yj in zip(y_idx, ykern):
-                      xyw = xi*yj*wc
-                      # wgt_grid[c, i, j] += xyw
-                      vis_grid[c, i, j] += xyw * vis[c]
-        
-        # now the FFTs
-        # the *ngx*ngy corrects for the FFT normalisation
-        dirty = Fs(ifft2(iFs(vis_grid))*ngx*ngy)[:, slcx, slcy]
-        dirty /= (xcorrector[slcx, None] * ycorrector[None, slcy])
+        for c in range(ncorr):
+          wc = wgt[c]
+          for i, xi in zip(x_idx, xkern):
+            for j, yj in zip(y_idx, ykern):
+              xyw = xi * yj * wc
+              # wgt_grid[c, i, j] += xyw
+              vis_grid[c, i, j] += xyw * vis[c]
 
-        return dirty
-    
-    # _impl.returns = types.Tuple([types.Array(types.complex128, 3, 'C'),
-    #                              types.Array(types.float64, 3, 'C')])
-    return _impl
+    # now the FFTs
+    # the *ngx*ngy corrects for the FFT normalisation
+    dirty = Fs(ifft2(iFs(vis_grid)) * ngx * ngy)[:, slcx, slcy]
+    dirty /= xcorrector[slcx, None] * ycorrector[None, slcy]
 
-def stokes_funcs(data, jones, product, pol, nc):
-  # set up symbolic expressions
-  gp00, gp10, gp01, gp11 = sm.symbols("gp00 gp10 gp01 gp11", real=False)
-  gq00, gq10, gq01, gq11 = sm.symbols("gq00 gq10 gq01 gq11", real=False)
-  w0, w1, w2, w3 = sm.symbols("W0 W1 W2 W3", real=True)
-  v00, v10, v01, v11 = sm.symbols("v00 v10 v01 v11", real=False)
+    return dirty
 
-  # Jones matrices
-  Gp = sm.Matrix([[gp00, gp01], [gp10, gp11]])
-  Gq = sm.Matrix([[gq00, gq01], [gq10, gq11]])
-
-  # Mueller matrix (row major form)
-  Mpq = TensorProduct(Gp, Gq.conjugate())
-  Mpqinv = TensorProduct(Gp.inv(), Gq.conjugate().inv())
-
-  # inverse noise covariance
-  Sinv = sm.Matrix([[w0, 0, 0, 0], [0, w1, 0, 0], [0, 0, w2, 0], [0, 0, 0, w3]])
-  S = Sinv.inv()
-
-  # visibilities
-  Vpq = sm.Matrix([[v00], [v01], [v10], [v11]])
-
-  # Full Stokes to corr operator
-  # Is this the only difference between linear and circular pol?
-  # What about paralactic angle rotation?
-  if pol.literal_value == "linear":
-    T = sm.Matrix(
-      [[1.0, 1.0, 0, 0], [0, 0, 1.0, 1.0j], [0, 0, 1.0, -1.0j], [1.0, -1.0, 0, 0]]
-    )
-  elif pol.literal_value == "circular":
-    T = sm.Matrix(
-      [[1.0, 0, 0, 1.0], [0, 1.0, 1.0j, 0], [0, 1.0, -1.0j, 0], [1.0, 0, 0, -1.0]]
-    )
-  Tinv = T.inv()
-
-  # Full Stokes weights
-  W = T.H * Mpq.H * Sinv * Mpq * T
-  Winv = Tinv * Mpqinv * S * Mpqinv.H * Tinv.H
-
-  # Full Stokes coherencies
-  C = Winv * (T.H * (Mpq.H * (Sinv * Vpq)))
-  # Only keep diagonal of weights
-  W = W.diagonal().T  # diagonal() returns row vector
-
-    # this should ensure that outputs are always ordered as
-    # [I, Q, U, V]
-    i = ()
-    if 'I' in product.literal_value:
-        i += (0,)
-
-    if 'Q' in product.literal_value:
-        i += (1,)
-        if pol.literal_value == 'circular' and nc.literal_value == '2':
-            raise ValueError("Q is not available in circular polarisation with 2 correlations")
-
-    if 'U' in product.literal_value:
-        i += (2,)
-        if pol.literal_value == 'linear' and nc.literal_value == '2':
-            raise ValueError("U is not available in linear polarisation with 2 correlations")
-        elif pol.literal_value == 'circular' and nc.literal_value == '2':
-            raise ValueError("U is not available in circular polarisation with 2 correlations")
-
-    if 'V' in product.literal_value:
-        i += (3,)
-        if pol.literal_value == 'linear' and nc.literal_value == '2':
-            raise ValueError("V is not available in linear polarisation with 2 correlations")
-
-    remprod = product.literal_value.strip('IQUV')
-    if len(remprod):
-        raise ValueError(f"Unknown polarisation product {remprod}")
-
-  else:
-    raise ValueError("Jones term has incorrect number of dimensions")
-
-  return vfunc, wfunc
+  # _impl.returns = types.Tuple([types.Array(types.complex128, 3, 'C'),
+  #                              types.Array(types.float64, 3, 'C')])
+  return _impl
---
+++ radiomesh/stokes.py
@@ -0,0 +1,256 @@
+import sympy as sm
+from numba import njit
+from sympy.physics.quantum import TensorProduct
+from sympy.utilities.lambdify import lambdify
+
+
+def stokes_funcs(data, jones, product, pol, nc):
+  # set up symbolic expressions
+  gp00, gp10, gp01, gp11 = sm.symbols("gp00 gp10 gp01 gp11", real=False)
+  gq00, gq10, gq01, gq11 = sm.symbols("gq00 gq10 gq01 gq11", real=False)
+  w0, w1, w2, w3 = sm.symbols("W0 W1 W2 W3", real=True)
+  v00, v10, v01, v11 = sm.symbols("v00 v10 v01 v11", real=False)
+
+  # Jones matrices
+  Gp = sm.Matrix([[gp00, gp01], [gp10, gp11]])
+  Gq = sm.Matrix([[gq00, gq01], [gq10, gq11]])
+
+  # Mueller matrix (row major form)
+  Mpq = TensorProduct(Gp, Gq.conjugate())
+  Mpqinv = TensorProduct(Gp.inv(), Gq.conjugate().inv())
+
+  # inverse noise covariance
+  Sinv = sm.Matrix([[w0, 0, 0, 0], [0, w1, 0, 0], [0, 0, w2, 0], [0, 0, 0, w3]])
+  S = Sinv.inv()
+
+  # visibilities
+  Vpq = sm.Matrix([[v00], [v01], [v10], [v11]])
+
+  # Full Stokes to corr operator
+  # Is this the only difference between linear and circular pol?
+  # What about paralactic angle rotation?
+  if pol.literal_value == "linear":
+    T = sm.Matrix(
+      [[1.0, 1.0, 0, 0], [0, 0, 1.0, 1.0j], [0, 0, 1.0, -1.0j], [1.0, -1.0, 0, 0]]
+    )
+  elif pol.literal_value == "circular":
+    T = sm.Matrix(
+      [[1.0, 0, 0, 1.0], [0, 1.0, 1.0j, 0], [0, 1.0, -1.0j, 0], [1.0, 0, 0, -1.0]]
+    )
+  Tinv = T.inv()
+
+  # Full Stokes weights
+  W = T.H * Mpq.H * Sinv * Mpq * T
+  Winv = Tinv * Mpqinv * S * Mpqinv.H * Tinv.H
+
+  # Full Stokes coherencies
+  C = Winv * (T.H * (Mpq.H * (Sinv * Vpq)))
+  # Only keep diagonal of weights
+  W = W.diagonal().T  # diagonal() returns row vector
+
+  # this should ensure that outputs are always ordered as
+  # [I, Q, U, V]
+  i = ()
+  if "I" in product.literal_value:
+    i += (0,)
+
+  if "Q" in product.literal_value:
+    i += (1,)
+    if pol.literal_value == "circular" and nc.literal_value == "2":
+      raise ValueError(
+        "Q is not available in circular polarisation with 2 correlations"
+      )
+
+  if "U" in product.literal_value:
+    i += (2,)
+    if pol.literal_value == "linear" and nc.literal_value == "2":
+      raise ValueError("U is not available in linear polarisation with 2 correlations")
+    elif pol.literal_value == "circular" and nc.literal_value == "2":
+      raise ValueError(
+        "U is not available in circular polarisation with 2 correlations"
+      )
+
+  if "V" in product.literal_value:
+    i += (3,)
+    if pol.literal_value == "linear" and nc.literal_value == "2":
+      raise ValueError("V is not available in linear polarisation with 2 correlations")
+
+  remprod = product.literal_value.strip("IQUV")
+  if len(remprod):
+    raise ValueError(f"Unknown polarisation product {remprod}")
+
+  if jones.ndim == 6:  # Full mode
+    Wsymb = lambdify(
+      (gp00, gp01, gp10, gp11, gq00, gq01, gq10, gq11, w0, w1, w2, w3),
+      sm.simplify(W[i, 0]),
+    )
+    Wjfn = njit(nogil=True, inline="always")(Wsymb)
+
+    Dsymb = lambdify(
+      (
+        gp00,
+        gp01,
+        gp10,
+        gp11,
+        gq00,
+        gq01,
+        gq10,
+        gq11,
+        w0,
+        w1,
+        w2,
+        w3,
+        v00,
+        v01,
+        v10,
+        v11,
+      ),
+      sm.simplify(C[i, 0]),
+    )
+    Djfn = njit(nogil=True, inline="always")(Dsymb)
+
+    @njit(nogil=True, inline="always")
+    def wfunc(gp, gq, W):
+      gp00 = gp[0, 0]
+      gp01 = gp[0, 1]
+      gp10 = gp[1, 0]
+      gp11 = gp[1, 1]
+      gq00 = gq[0, 0]
+      gq01 = gq[0, 1]
+      gq10 = gq[1, 0]
+      gq11 = gq[1, 1]
+      W00 = W[0]
+      W01 = W[1]
+      W10 = W[2]
+      W11 = W[3]
+      return Wjfn(
+        gp00, gp01, gp10, gp11, gq00, gq01, gq10, gq11, W00, W01, W10, W11
+      ).real.ravel()
+
+    @njit(nogil=True, inline="always")
+    def vfunc(gp, gq, W, V):
+      gp00 = gp[0, 0]
+      gp01 = gp[0, 1]
+      gp10 = gp[1, 0]
+      gp11 = gp[1, 1]
+      gq00 = gq[0, 0]
+      gq01 = gq[0, 1]
+      gq10 = gq[1, 0]
+      gq11 = gq[1, 1]
+      W00 = W[0]
+      W01 = W[1]
+      W10 = W[2]
+      W11 = W[3]
+      V00 = V[0]
+      V01 = V[1]
+      V10 = V[2]
+      V11 = V[3]
+      return Djfn(
+        gp00,
+        gp01,
+        gp10,
+        gp11,
+        gq00,
+        gq01,
+        gq10,
+        gq11,
+        W00,
+        W01,
+        W10,
+        W11,
+        V00,
+        V01,
+        V10,
+        V11,
+      ).ravel()
+
+  elif jones.ndim == 5:  # DIAG mode
+    W = W.subs(gp10, 0)
+    W = W.subs(gp01, 0)
+    W = W.subs(gq10, 0)
+    W = W.subs(gq01, 0)
+    C = C.subs(gp10, 0)
+    C = C.subs(gp01, 0)
+    C = C.subs(gq10, 0)
+    C = C.subs(gq01, 0)
+
+    Wsymb = lambdify((gp00, gp11, gq00, gq11, w0, w1, w2, w3), sm.simplify(W[i, 0]))
+    Wjfn = njit(nogil=True, inline="always")(Wsymb)
+
+    Dsymb = lambdify(
+      (gp00, gp11, gq00, gq11, w0, w1, w2, w3, v00, v01, v10, v11), sm.simplify(C[i, 0])
+    )
+    Djfn = njit(nogil=True, inline="always")(Dsymb)
+
+    if nc.literal_value == "4":
+
+      @njit(nogil=True, inline="always")
+      def wfunc(gp, gq, W):
+        gp00 = gp[0]
+        gp11 = gp[1]
+        gq00 = gq[0]
+        gq11 = gq[1]
+        W00 = W[0]
+        W01 = W[1]
+        W10 = W[2]
+        W11 = W[3]
+        return Wjfn(gp00, gp11, gq00, gq11, W00, W01, W10, W11).real.ravel()
+
+      @njit(nogil=True, inline="always")
+      def vfunc(gp, gq, W, V):
+        gp00 = gp[0]
+        gp11 = gp[1]
+        gq00 = gq[0]
+        gq11 = gq[1]
+        W00 = W[0]
+        W01 = W[1]
+        W10 = W[2]
+        W11 = W[3]
+        V00 = V[0]
+        V01 = V[1]
+        V10 = V[2]
+        V11 = V[3]
+        return Djfn(
+          gp00, gp11, gq00, gq11, W00, W01, W10, W11, V00, V01, V10, V11
+        ).ravel()
+    elif nc.literal_value == "2":
+
+      @njit(nogil=True, inline="always")
+      def wfunc(gp, gq, W):
+        gp00 = gp[0]
+        gp11 = gp[1]
+        gq00 = gq[0]
+        gq11 = gq[1]
+        W00 = W[0]
+        W01 = 1.0
+        W10 = 1.0
+        W11 = W[-1]
+        return Wjfn(gp00, gp11, gq00, gq11, W00, W01, W10, W11).real.ravel()
+
+      @njit(nogil=True, inline="always")
+      def vfunc(gp, gq, W, V):
+        gp00 = gp[0]
+        gp11 = gp[1]
+        gq00 = gq[0]
+        gq11 = gq[1]
+        W00 = W[0]
+        W01 = 1.0
+        W10 = 1.0
+        W11 = W[-1]
+        V00 = V[0]
+        V01 = 0j
+        V10 = 0j
+        V11 = V[-1]
+        return Djfn(
+          gp00, gp11, gq00, gq11, W00, W01, W10, W11, V00, V01, V10, V11
+        ).ravel()
+    else:
+      raise ValueError(
+        f"Selected product is only available from 2 or 4"
+        f"correlation data while you have ncorr={nc}."
+      )
+
+  else:
+    raise ValueError("Jones term has incorrect number of dimensions")
+
+  return vfunc, wfunc
