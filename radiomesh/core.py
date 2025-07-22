2564f59190cfea0039f35df4896155a644e8dd6c
landmanbester <lbester@sarao.ac.za>
2025-07-22 18:10:40 +0200

    test wterm as convolution (eqn 10)

--- radiomesh/core.py
+++ radiomesh/core.py
@@ -20,36 +20,70 @@ JIT_OPTIONS = {"nogil": True, "cache": True, "error_model": "numpy", "fastmath":
 
 
 # hardcode 2D gridding params
-# support, padding, epsilon, beta, alpha
+# alpha (support), sigma (padding), epsilon, beta, mu
 # 8, 1.40, 3.1679034e-05, 1.83155364990234371, 0.516968027750650871  (single precision)
 # 8, 1.40, 8.5117152e-06, 1.82943505181206612, 0.517185719807942368  (double precision)
 
 
-def grid_corrector(ng, supp, beta, alpha, nroots=32):
+# TODO - the grid correctors could be further optimized
+# see discussion below eq 3.10 in https://arxiv.org/pdf/1808.06736
+def grid_corrector2D(dom, alpha, beta, mu, nroots=32):
   # even number of roots required to exploit even symmetry of integrand
   if nroots % 2:
     nroots += 1
-  dom = np.linspace(-0.5, 0.5, ng, endpoint=False)
+  q, wgt = np.polynomial.legendre.leggauss(nroots)
+  idx = q > 0
+  q = q[idx]
+  wgt = wgt[idx]
+  z = np.einsum("ij,k->ijk", dom, q)
+  xkern = es_kernel(q * alpha / 2, beta, mu, alpha)
+  tmp = np.sum(
+    np.cos(-np.pi * alpha * z) * xkern[None, None, :] * wgt[None, None, :], axis=-1
+  )
+  return alpha * tmp
+
+
+def grid_corrector(dom, alpha, beta, mu, nroots=32):
+  # even number of roots required to exploit even symmetry of integrand
+  if nroots % 2:
+    nroots += 1
   q, wgt = np.polynomial.legendre.leggauss(nroots)
   idx = q > 0
   q = q[idx]
   wgt = wgt[idx]
   z = np.outer(dom, q)
   xkern = np.zeros(q.size)
-  _es_kernel(q, xkern, supp * beta, alpha)
-  tmp = np.sum(np.cos(np.pi * supp * z) * xkern[None, :] * wgt[None, :], axis=-1)
-  return supp * tmp
+  _es_kernel(q, xkern, alpha * beta, mu)
+  tmp = np.sum(np.cos(-np.pi * alpha * z) * xkern[None, :] * wgt[None, :], axis=-1)
+  return alpha * tmp
 
 
 @njit(**JIT_OPTIONS, inline="always")
-def _es_kernel(x, kern, betak, alphak):
-  # oneminxsq = (1 - x) * (1 + x)
-  # mx = oneminxsq <= 1
-  # kern[mx] = np.exp(betak * (np.pow(oneminxsq[mx], alphak) - 1))
-  # kern[~mx] = 0.0
+def _es_kernel(x, kern, betak, mu):
+  """
+  Scaled version of the kernel
+
+  exp(betak * (power(1 - x ** 2, mu) - 1))
+
+  i.e. support squashed to [-1, 1] with betak = beta * alpha
+  """
   kern[...] = 0.0
   mask = np.abs(x) <= 1
-  kern[mask] = np.exp(betak * (np.sqrt(1 - x[mask] ** 2) - 1))
+  kern[mask] = np.exp(betak * (np.power(1 - x[mask] ** 2, mu) - 1))
+  return kern
+
+
+@njit(**JIT_OPTIONS)
+def es_kernel(x, beta, mu, alpha):
+  """
+  Unscaled version of the gridding
+
+  exp(alpha * beta * (power(1 - (2*x/alpha) ** 2, mu) - 1))
+
+  i.e. x is in pixel units
+  """
+  kern = np.zeros_like(x, dtype=x.dtype)
+  _es_kernel(2 * x / alpha, kern, beta * alpha, mu)
   return kern
 
 
@@ -108,10 +142,10 @@ def _grid_data_impl(
   pol: str,
   product: str,
   nc: int,
-  padding=2.0,
-  supp=8,
+  sigma=2.0,
+  alpha=8,
   beta=2.3,
-  alpha=0.5,
+  mu=0.5,
 ):
   raise NotImplementedError
 
@@ -135,10 +169,10 @@ def nb_grid_data_impl(
   pol,
   product,
   nc,
-  padding=2.0,
-  supp=8,
+  sigma=2.0,
+  alpha=8,
   beta=2.3,
-  alpha=0.5,
+  mu=0.5,
 ):
   if not isinstance(pol, types.StringLiteral):
     raise RequireLiteralValue(f"'pol' {pol} is not a str literal")
@@ -168,10 +202,10 @@ def nb_grid_data_impl(
     pol,
     product,
     nc,
-    padding=2.0,
-    supp=8,
+    sigma=2.0,
+    alpha=8,
     beta=2.3,
-    alpha=0.5,
+    mu=0.5,
   ):
     ntime, nbl, nchan, ncorr = data.shape
     vis_grid = np.zeros((ns, ngx, ngy), dtype=data.dtype)
@@ -185,11 +219,11 @@ def nb_grid_data_impl(
     vmax = 1 / celly / 2
 
     normfreq = freq / LIGHTSPEED
-    ko2 = supp / 2
-    betak = beta * supp
-    pos = (np.arange(supp) - ko2).astype(np.int64)
-    xkern = np.zeros(supp)
-    ykern = np.zeros(supp)
+    half_supp = alpha / 2
+    betak = beta * alpha
+    pos = (np.arange(alpha) - half_supp).astype(np.int64)
+    xkern = np.zeros(alpha)
+    ykern = np.zeros(alpha)
     for t in range(ntime):
       for bl in range(nbl):
         p = int(ant1[bl])
@@ -218,14 +252,14 @@ def nb_grid_data_impl(
 
           # the grid position is pos + u_idx on the un-fftshifted grid
           x_idx = pos + u_idx
-          x = (x_idx - ug) / ko2
+          x = (x_idx - ug) / half_supp
           # this finds the index on the fftshifted grid
           # x_idx = (x_idx + ngx//2) % ngx
-          _es_kernel(x, xkern, betak, alpha)
+          _es_kernel(x, xkern, betak, mu)
           y_idx = pos + v_idx
-          y = (y_idx - vg) / ko2
+          y = (y_idx - vg) / half_supp
           # y_idx = (y_idx + ngy//2) % ngy
-          _es_kernel(y, ykern, betak, alpha)
+          _es_kernel(y, ykern, betak, mu)
 
           for c in range(ncorr):
             wc = wgt[c]
@@ -261,14 +295,15 @@ def grid_data_np(
   nc: int,
   vis_func: Callable,
   wgt_func: Callable,
-  padding=2.0,
-  supp=10,
+  sigma=2.0,
+  alpha=10,
   beta=2.3,
-  alpha=0.5,
+  mu=0.5,
   usign=1,
   vsign=-1,
 ):
   ntime, nbl, nchan, ncorr = data.shape
+  # we only need half the
   vis_grid = np.zeros((nc, ngx, ngy), dtype=data.dtype)
 
   # ufreq
@@ -280,11 +315,11 @@ def grid_data_np(
   vmax = 1 / celly / 2
 
   normfreq = freq / LIGHTSPEED
-  ko2 = supp / 2
-  betak = beta * supp
-  pos = (np.arange(supp) - ko2).astype(np.int64)
-  xkern = np.zeros(supp)
-  ykern = np.zeros(supp)
+  half_supp = alpha / 2
+  betak = beta * alpha
+  pos = (np.arange(alpha) - half_supp).astype(np.int64)
+  xkern = np.zeros(alpha)
+  ykern = np.zeros(alpha)
   for t in range(ntime):
     for bl in range(nbl):
       p = int(ant1[bl])
@@ -313,14 +348,14 @@ def grid_data_np(
 
         # the grid position is pos + u_idx on the un-fftshifted grid
         x_idx = pos + u_idx
-        x = (x_idx - ug) / ko2
+        x = (x_idx - ug) / half_supp
         # this finds the index on the fftshifted grid
         # x_idx = (x_idx + ngx//2) % ngx
-        _es_kernel(x, xkern, betak, alpha)
+        _es_kernel(x, xkern, betak, mu)
         y_idx = pos + v_idx
-        y = (y_idx - vg) / ko2
+        y = (y_idx - vg) / half_supp
         # y_idx = (y_idx + ngy//2) % ngy
-        _es_kernel(y, ykern, betak, alpha)
+        _es_kernel(y, ykern, betak, mu)
 
         for c in range(ncorr):
           wc = wgt[c]
@@ -348,30 +383,30 @@ def vis2im(
   pol: str,
   product: str,
   nc: int,
-  padding=2.0,
-  supp=10,
+  sigma=2.0,
+  alpha=10,
   beta=2.3,
-  alpha=0.5,
+  mu=0.5,
 ):
-  ngx = good_size(int(padding * nx))
+  ngx = good_size(int(sigma * nx))
   # make sure it is even and a good size for the FFT
   while ngx % 2:
     ngx = good_size(ngx + 1)
-  xcorrector = grid_corrector(ngx, supp, beta, alpha)
+  xcorrector = grid_corrector(ngx, alpha, beta, mu)
   padxl = (ngx - nx) // 2
   padxr = ngx - nx - padxl
   slcx = slice(padxl, -padxr)
-  ngy = good_size(int(padding * ny))
+  ngy = good_size(int(sigma * ny))
   # make sure it is even and a good size for the FFT
   while ngy % 2:
     ngy = good_size(ngy + 1)
-  ycorrector = grid_corrector(ngy, supp, beta, alpha)
+  ycorrector = grid_corrector(ngy, alpha, beta, mu)
   padyl = (ngy - ny) // 2
   padyr = ngy - ny - padyl
   slcy = slice(padyl, -padyr)
 
   # taper
-  corrector = xcorrector[None, :, None] * ycorrector[None, None, :]
+  corrector = xcorrector[slcx, None] * ycorrector[None, slcy]
 
   vis_func, wgt_func = stokes_funcs(jones, product, pol, nc)
 
@@ -393,10 +428,10 @@ def vis2im(
     nc,
     vis_func,
     wgt_func,
-    padding=2.0,
-    supp=10,
+    sigma=2.0,
+    alpha=10,
     beta=2.3,
-    alpha=0.5,
+    mu=0.5,
     usign=1,
     vsign=1,
   )
@@ -404,7 +439,238 @@ def vis2im(
   # now the FFTs
   # the *ngx*ngy corrects for the FFT normalisation
   dirty = np.fft.ifft2(iFs(grid), axes=(-2, -1)) * ngx * ngy
-  dirty = Fs(dirty.real)
+  dirty = Fs(dirty.real)[:, slcx, slcy]
   # apply taper
   dirty /= corrector
-  return dirty[:, slcx, slcy]
+  return dirty
+
+
+@njit(**JIT_OPTIONS)
+def wgrid_data_np(
+  uvw: npt.NDArray,
+  freq: npt.NDArray,
+  data: npt.NDArray,
+  weight: npt.NDArray,
+  flag: npt.NDArray,
+  jones: npt.NDArray,
+  ant1: npt.NDArray,
+  ant2: npt.NDArray,
+  ngx: int,
+  ngy: int,
+  cellx: float,
+  celly: float,
+  pol: str,
+  product: str,
+  nc: int,
+  vis_func: Callable,
+  wgt_func: Callable,
+  w0: float,
+  dw: float,
+  nw: int,
+  sigma=2.0,
+  alpha=10,
+  beta=2.3,
+  mu=0.5,
+  usign=1,
+  vsign=1,
+  wsign=-1,
+):
+  ntime, nbl, nchan, ncorr = data.shape
+  # create a grid per wplane
+  vis_grid = np.zeros((nc, nw, ngx, ngy), dtype=data.dtype)
+
+  # ufreq
+  u_cell = 1 / (ngx * cellx)
+  umax = 1 / cellx / 2
+
+  # vfreq
+  v_cell = 1 / (ngy * celly)
+  vmax = 1 / celly / 2
+
+  # wfreq
+  w_cell = dw
+  wmax = w0 + (nw - 1) * dw
+  wgrid = w0 + np.arange(nw) * dw
+
+  normfreq = freq / LIGHTSPEED
+  half_supp = alpha / 2
+  betak = beta * alpha
+  pos = (np.arange(alpha) - half_supp).astype(np.int64)
+  xkern = np.zeros(alpha)
+  ykern = np.zeros(alpha)
+  zkern = np.zeros(alpha)
+  for t in range(ntime):
+    for bl in range(nbl):
+      p = int(ant1[bl])
+      q = int(ant2[bl])
+      gp = jones[t, p, :, 0]
+      gq = jones[t, q, :, 0]
+      uvw_row = uvw[t, bl]
+      wgt_row = weight[t, bl]
+      vis_row = data[t, bl]
+      for chan in range(nchan):
+        if flag[t, bl, chan].any():
+          continue
+        wgt = wgt_func(gp[chan], gq[chan], wgt_row[chan])
+        vis = vis_func(gp[chan], gq[chan], wgt_row[chan], vis_row[chan])
+
+        # current uv coords
+        chan_normfreq = normfreq[chan]
+        u_tmp = uvw_row[0] * chan_normfreq * usign
+        v_tmp = uvw_row[1] * chan_normfreq * vsign
+        w_tmp = uvw_row[1] * chan_normfreq * wsign
+        # only use half the w grid due to Hermitian symmetry
+        if w_tmp < 0:
+          u_tmp = -u_tmp
+          v_tmp = -v_tmp
+          w_tmp = -w_tmp
+          vis = np.conjugate(vis)
+
+        # pixel coordinates
+        ug = (u_tmp + umax) / u_cell
+        vg = (v_tmp + vmax) / v_cell
+        wg = (w_tmp + wmax) / w_cell
+
+        # indices
+        u_idx = int(np.round(ug))
+        v_idx = int(np.round(vg))
+        w_idx = int(np.round(wg))
+
+        # the grid position is pos + u_idx on the un-fftshifted grid
+        x_idx = pos + u_idx
+        x = (x_idx - ug) / half_supp
+        # this finds the index on the fftshifted grid
+        # x_idx = (x_idx + ngx//2) % ngx
+        _es_kernel(x, xkern, betak, mu)
+
+        y_idx = pos + v_idx
+        y = (y_idx - vg) / half_supp
+        # y_idx = (y_idx + ngy//2) % ngy
+        _es_kernel(y, ykern, betak, mu)
+
+        w_diff = np.abs(wgrid - w_tmp)
+        w_idx = np.nonzero(w_diff == w_diff.min())[0]
+        z_idx = pos + w_idx
+        # z = (z_idx - wg) / half_supp
+        z = (wgrid - w_tmp) / dw / half_supp
+        _es_kernel(z, zkern, betak, mu)
+
+        # import ipdb; ipdb.set_trace()
+
+        for c in range(ncorr):
+          wc = wgt[c]
+          for k, zk in zip(z_idx, zkern):
+            for i, xi in zip(x_idx, xkern):
+              for j, yj in zip(y_idx, ykern):
+                xyzwc = xi * yj * zk * wc
+                vis_grid[c, k, i, j] += xyzwc * vis[c]
+
+  return vis_grid
+
+
+def vis2im_wgrid(
+  uvw: npt.NDArray,
+  freq: npt.NDArray,
+  data: npt.NDArray,
+  weight: npt.NDArray,
+  flag: npt.NDArray,
+  jones: npt.NDArray,
+  ant1: npt.NDArray,
+  ant2: npt.NDArray,
+  nx: int,
+  ny: int,
+  cellx: float,
+  celly: float,
+  pol: str,
+  product: str,
+  nc: int,
+  sigma=2,
+  alpha=10,
+  beta=2.3,
+  mu=0.5,
+):
+  ngx = good_size(int(sigma * nx))
+  # make sure it is even and a good size for the FFT
+  while ngx % 2:
+    ngx = good_size(ngx + 1)
+  xcorrector = grid_corrector(ngx, alpha, beta, mu)
+  padxl = (ngx - nx) // 2
+  padxr = ngx - nx - padxl
+  slcx = slice(padxl, -padxr)
+  ngy = good_size(int(sigma * ny))
+  while ngy % 2:
+    ngy = good_size(ngy + 1)
+  ycorrector = grid_corrector(ngy, alpha, beta, mu)
+  padyl = (ngy - ny) // 2
+  padyr = ngy - ny - padyl
+  slcy = slice(padyl, -padyr)
+
+  # xy taper
+  corrector = xcorrector[slcx, None] * ycorrector[None, slcy]
+
+  # get number of w grids
+  wmin = np.abs(uvw[:, :, 2].ravel() * freq.min() / LIGHTSPEED).min()
+  wmax = np.abs(uvw[:, :, 2].ravel() * freq.max() / LIGHTSPEED).max()
+  x, y = np.meshgrid(*[-ss / 2 + np.arange(ss) for ss in (nx, ny)], indexing="ij")
+  x *= cellx
+  y *= celly
+  eps = x**2 + y**2
+  nm1 = -eps / (np.sqrt(1.0 - eps) + 1.0)
+  # missing factor of a half compared to expression in the paper but
+  # this gives the same w parameters as reported by the wgridder
+  dw = 1 / (sigma * np.abs(nm1).max())
+  nw = int(np.round((wmax - wmin) / dw)) + alpha
+  # different from expression in paper?
+  w0 = (wmin + wmax) / 2 - dw * (nw - 1) / 2
+  wcorrector = grid_corrector2D(nm1 * dw, alpha, beta, mu) * (nm1 + 1)
+
+  vis_func, wgt_func = stokes_funcs(jones, product, pol, nc)
+
+  grid = wgrid_data_np(
+    uvw,
+    freq,
+    data,
+    weight,
+    flag,
+    jones,
+    ant1,
+    ant2,
+    ngx,
+    ngy,
+    cellx,
+    celly,
+    pol,
+    product,
+    nc,
+    vis_func,
+    wgt_func,
+    w0,
+    dw,
+    nw,
+    sigma=sigma,
+    alpha=alpha,
+    beta=beta,
+    mu=mu,
+    usign=1,
+    vsign=1,
+    wsign=-1,
+  )
+  # 2D FFTs
+  # the *ngx*ngy corrects for the FFT normalisation
+  dirty = np.fft.ifft2(iFs(grid), axes=(-2, -1)) * ngx * ngy
+  dirty = Fs(dirty)[:, :, slcx, slcy]
+
+  # w-screens
+  wgrid = w0 + np.arange(nw) * dw
+  wscreens = np.exp(-2j * np.pi * nm1[None, :, :] * wgrid[:, None, None]).astype(
+    data.dtype
+  )
+  dirty *= wscreens[None, :, :, :]
+  # sum over w axis
+  dirty = dirty.real.sum(axis=1)
+  # xy tapers
+  dirty /= corrector[None, :, :]
+  # w-taper * n
+
+  dirty /= wcorrector
+  return dirty
--- radiomesh/tests/test_core.py
+++ radiomesh/tests/test_core.py
@@ -4,9 +4,16 @@ import numpy as np
 import numpy.typing as npt
 import pytest
 import xarray as xr
+from numpy.testing import assert_array_almost_equal
 
 from radiomesh.constants import LIGHTSPEED
-from radiomesh.core import vis2im
+from radiomesh.core import (
+  _es_kernel,
+  es_kernel,
+  grid_corrector,
+  vis2im,
+  vis2im_wgrid,
+)
 from radiomesh.stokes import stokes_funcs
 
 pmp = pytest.mark.parametrize
@@ -35,7 +42,11 @@ def explicit_gridder(
   eps = x**2 + y**2
   if apply_w:
     nm1 = -eps / (np.sqrt(1.0 - eps) + 1.0)
-    n = nm1 + 1
+    n = (nm1 + 1)[
+      None,
+      :,
+      :,
+    ]
   else:
     nm1 = 0.0
     n = 1.0
@@ -50,18 +61,193 @@ def explicit_gridder(
       for chan in range(nchan):
         if flag[t, bl, chan].any():
           continue
-        phase = (
-          freq[chan]
-          / LIGHTSPEED
-          * (x * uvw[t, bl, 0] + y * uvw[t, bl, 1] - uvw[t, bl, 2] * nm1)
-        )
-        cphase = np.exp(2j * np.pi * phase)
         # convert to corrected Stokes vis and weights
         vis = vis_func(gp[chan, 0], gq[chan, 0], weight[t, bl, chan], data[t, bl, chan])
         wgt = wgt_func(gp[chan, 0], gq[chan, 0], weight[t, bl, chan])
+        u, v, w = uvw[t, bl]
+        if w < 0:
+          u *= -1
+          v *= -1
+          w *= -1
+          vis = np.conjugate(vis)
+        phase = freq[chan] / LIGHTSPEED * (x * u + y * v - w * nm1)
+        cphase = np.exp(2j * np.pi * phase)
         for corr in range(ncorr):
           res[corr] += (vis[corr] * wgt[corr] * cphase).real
   return res / n
+
+
+def taper_trapz(dom, alpha=5, beta=2.3, mu=0.5):
+  npix = dom.size
+  res = np.zeros(npix, dtype=np.float64)
+  alphao2 = alpha / 2
+  x = np.linspace(-alphao2, alphao2, npix)
+  kern = np.zeros_like(x)
+  betak = alpha * beta
+  for i, k in enumerate(dom):
+    kern[...] = 0.0
+    tmp = np.cos(-2 * np.pi * k * x) * _es_kernel(x / alphao2, kern, betak, mu)
+    res[i] = np.trapezoid(tmp, x)
+  return res
+
+
+def test_tapers(ms_name):
+  """
+  Compare Gauss-Legendre integration of kernel to trapz
+  """
+  fov = 1
+  sigma = 2.0
+  alpha = 10
+  beta = 2.3
+  mu = 0.5
+
+  dt = xr.open_datatree(ms_name, engine="xarray-ms:msv2")
+  dt_ms = dt[dt.groups[1]]
+  vis = dt_ms.VISIBILITY.values
+  vis[:, :, :, 0] = 1.0
+  vis[:, :, :, -1] = 1.0
+  freq = dt_ms.frequency.values
+  uvw = dt_ms.UVW.values
+
+  # Nyquist
+  umax = np.abs(uvw[:, :, 0]).max()
+  vmax = np.abs(uvw[:, :, 1]).max()
+  uv_max = np.maximum(umax, vmax)
+  cell = 1.0 / (2 * uv_max * freq.max() / LIGHTSPEED)
+  nx = int(np.ceil(np.deg2rad(fov) / cell))
+  if nx % 2:
+    nx += 1
+  ny = nx // 2
+  x = (-(nx // 2) + np.arange(nx)) * cell
+  y = (-(ny // 2) + np.arange(ny)) * cell
+
+  xtaper = grid_corrector(x, alpha, beta, mu)
+  xtaper_trapz = taper_trapz(x, alpha, beta, mu)
+  xdiff = np.abs(xtaper - xtaper_trapz)
+  ytaper = grid_corrector(y, alpha, beta, mu)
+  ytaper_trapz = taper_trapz(y, alpha, beta, mu)
+  ydiff = np.abs(ytaper - ytaper_trapz)
+
+  assert np.allclose(1 + xdiff, 1, atol=1e-10, rtol=1e-10)
+  assert np.allclose(1 + ydiff, 1, atol=1e-10, rtol=1e-10)
+
+  # grid coordinates
+  ngx = nx  # int(sigma*nx)
+  ngy = ny  # int(sigma*ny)
+  x, y = np.meshgrid(*[-ss / 2 + np.arange(ss) for ss in (ngx, ngy)], indexing="ij")
+  x *= cell
+  y *= cell
+
+  # get number of w grids
+  w = uvw[:, :, -1].ravel() * freq.max() / LIGHTSPEED
+  wabs = np.abs(w)
+  wmax = wabs.max()
+  wmin = -wmax
+
+  # this tests eqn 10 i.e. the grid corrector in the w-direction
+  eps = x**2 + y**2
+  nm1 = -eps / (np.sqrt(1.0 - eps) + 1.0)
+  ntilde = nm1.ravel()
+  dw = 1 / (2 * sigma * np.abs(ntilde).max())  # on the padded grid
+  nw = int(np.ceil((wmax - wmin) / dw)) + alpha
+  w0 = wmin - dw * (alpha - 1) / 2
+  wcorrector = grid_corrector(ntilde * dw, alpha, beta, mu)
+  wgrid = w0 + np.arange(nw) * dw
+  for ww in w[0::100]:
+    res = np.exp(-2j * np.pi * ww * ntilde)
+    z = (wgrid - ww) / dw
+    zkern = es_kernel(z, beta, mu, alpha)
+    tmp = np.exp(-2j * np.pi * ntilde[:, None] * wgrid[None, :])
+    tmp2 = tmp * zkern[None, :]
+    res2 = np.sum(tmp2, axis=-1)
+    # this is the same as because dc=dw
+    # res2 = np.trapezoid(tmp2/dw, wgrid, axis=-1)
+    res2 /= wcorrector
+    diff = np.abs(res - res2)
+    assert_array_almost_equal(1 + diff, 1.0, decimal=7)
+
+
+def test_eqn789(ms_name):
+  fov = 1.0
+  sigma = 2.0
+  alpha = 10
+  beta = 2.3
+  mu = 0.5
+
+  dt = xr.open_datatree(ms_name, engine="xarray-ms:msv2")
+  dt_ms = dt[dt.groups[1]]
+  vis = dt_ms.VISIBILITY.values
+  vis[:, :, :, 0] = 1.0
+  vis[:, :, :, -1] = 1.0
+  freq = dt_ms.frequency.values
+  uvw = dt_ms.UVW.values
+
+  umax = np.abs(uvw[:, :, 0]).max()
+  vmax = np.abs(uvw[:, :, 1]).max()
+  uv_max = np.maximum(umax, vmax)
+  cell = 1.0 / (2 * uv_max * freq.max() / LIGHTSPEED)
+  nx = int(np.ceil(np.deg2rad(fov) / cell))
+  if nx % 2:
+    nx += 1
+  ny = nx // 2
+
+  # grid coordinates
+  ngx = int(sigma * nx)
+  ngy = int(sigma * ny)
+  x, y = np.meshgrid(*[-ss / 2 + np.arange(ss) for ss in (ngx, ngy)], indexing="ij")
+  x *= cell
+  y *= cell
+
+  # get number of w grids
+  w = uvw[:, :, -1].ravel() * freq.max() / LIGHTSPEED
+  wabs = np.abs(w)
+  wmax = wabs.max()
+  wmin = -wmax
+
+  # this tests eqn 10 in the paper to test the 2D grid corrector for the w-direction
+  eps = x**2 + y**2
+  nm1 = -eps / (np.sqrt(1.0 - eps) + 1.0)
+  low = 0
+  high = ngx
+  ntilde = nm1.ravel()[low:high, None]
+  dw = 1 / (2 * np.abs(nm1).max())  # on the padded grid
+  w0 = wmin - dw * (alpha - 1) / 2
+  wf = wmax + dw * (alpha - 1) / 2
+  for ww in w[0::100]:
+    wterm = np.exp(-2j * np.pi * ww * ntilde)
+    # eqn 7 and 8 hold for any choice of k
+    k = np.random.random(ntilde.size) - 0.5
+    taper = grid_corrector(k, alpha, beta, mu)[:, None]
+    lhs = (wterm * taper).ravel()
+
+    # centered kernel eqn 7
+    wgrid = np.linspace(-alpha, alpha, nx)
+    tmp = np.exp(-2j * np.pi * k[:, None] * wgrid[None, :])
+    kern = es_kernel(wgrid, beta, mu, alpha)
+    tmp2 = wterm * kern[None, :] * tmp
+    rhs = np.trapezoid(tmp2, wgrid)
+    diff = np.abs(lhs - rhs)
+    assert_array_almost_equal(1 + diff, 1.0, decimal=6)
+
+    # uncentered kernel eqn 8
+    wgrid = np.linspace(w0, wf, nx)
+    z = (wgrid - ww) / dw
+    tmp = np.exp(-2j * np.pi * k[:, None] * z[None, :]) / dw
+    kern = es_kernel(z, beta, mu, alpha)
+    tmp2 = wterm * kern[None, :] * tmp
+    rhs = np.trapezoid(tmp2, wgrid)
+    diff = np.abs(lhs - rhs)
+    assert_array_almost_equal(1 + diff, 1.0, decimal=7)
+
+    # eqn 9 only holds when k overlaps with ntilde*dw
+    k = (ntilde * dw).ravel()
+    taper = grid_corrector(k, alpha, beta, mu)[:, None]
+    lhs = (wterm * taper).ravel()
+    tmp = np.exp(-2j * np.pi * ntilde * wgrid[None, :]) / dw
+    tmp2 = tmp * kern[None, :]
+    rhs = np.trapezoid(tmp2, wgrid)
+    diff = np.abs(lhs - rhs)
+    assert_array_almost_equal(1 + diff, 1.0, decimal=7)
 
 
 @pmp("fov", (1.0,))
@@ -110,23 +296,27 @@ def test_grid_data_now(fov, precision, ms_name):
     nx += 1
   ny = nx
 
-  dirty_dft = explicit_gridder(
-    uvw,
-    freq,
-    vis,
-    wgt,
-    flag,
-    jones,
-    ant1,
-    ant2,
-    nx,
-    ny,
-    cell,
-    cell,
-    False,
-    vis_func,
-    wgt_func,
-  )
+  try:
+    dirty_dft = np.load("radiomesh/tests/data/dirty_now.npy")
+  except Exception:
+    dirty_dft = explicit_gridder(
+      uvw,
+      freq,
+      vis,
+      wgt,
+      flag,
+      jones,
+      ant1,
+      ant2,
+      nx,
+      ny,
+      cell,
+      cell,
+      False,
+      vis_func,
+      wgt_func,
+    )
+    np.save("radiomesh/tests/data/dirty_now.npy", dirty_dft)
   dirty = vis2im(
     uvw,
     freq,
@@ -147,3 +337,108 @@ def test_grid_data_now(fov, precision, ms_name):
   # we compare fractional differences because abs values can be very large
   diff = (dirty - dirty_dft) / dirty_dft.max()
   assert np.allclose(1 + diff, 1, rtol=1e-4, atol=1e-4)
+
+
+@pytest.mark.skip(reason="Future Landman's problem")
+@pmp("fov", (1.0,))
+@pmp("precision", ("single",))
+def test_grid_data(fov, precision, ms_name):
+  np.random.seed(420)
+  if precision == "single":
+    # real_type = "f4"
+    complex_type = "c8"
+  else:
+    # real_type = "f8"
+    complex_type = "c16"
+
+  dt = xr.open_datatree(ms_name, engine="xarray-ms:msv2")
+  dt_ms = dt[dt.groups[1]]
+  dt_ant = dt[dt.groups[2]]
+  vis = dt_ms.VISIBILITY.values
+  vis[:, :, :, 0] = 1.0
+  vis[:, :, :, -1] = 1.0
+  wgt = dt_ms.WEIGHT.values
+  flag = dt_ms.FLAG.values
+  freq = dt_ms.frequency.values
+  uvw = dt_ms.UVW.values
+  ntime, nbl, nchan, ncorr = vis.shape
+  nant = dt_ant.antenna_name.size
+  jones = np.zeros((ntime, nant, nchan, 1, 2, 2), dtype=complex_type)
+  jones[:, :, :, :, 0, 0] = 1.0
+  jones[:, :, :, :, 1, 1] = 1.0
+  _, ant1 = np.unique(dt_ms.baseline_antenna1_name.values, return_inverse=True)
+  _, ant2 = np.unique(dt_ms.baseline_antenna2_name.values, return_inverse=True)
+  pols = dt_ms.polarization.values
+  if "XX" in pols or "YY" in pols:
+    pol = "linear"
+  elif "RR" in pols or "LL" in pols:
+    pol = "circular"
+  product = "IQUV"
+  vis_func, wgt_func = stokes_funcs(jones, product, pol, ncorr)
+
+  # if we don't grid at Nyquist some uv points may fall off the grid
+  umax = np.abs(uvw[:, :, 0]).max()
+  vmax = np.abs(uvw[:, :, 1]).max()
+  uv_max = np.maximum(umax, vmax)
+  cell = 1.0 / (2 * uv_max * freq.max() / LIGHTSPEED)
+  nx = int(np.ceil(np.deg2rad(fov) / cell))
+  if nx % 2:
+    nx += 1
+  ny = nx
+
+  try:
+    dirty_dft = np.load("radiomesh/tests/data/dirty.npy")
+  except Exception:
+    dirty_dft = explicit_gridder(
+      uvw,
+      freq,
+      vis,
+      wgt,
+      flag,
+      jones,
+      ant1,
+      ant2,
+      nx,
+      ny,
+      cell,
+      cell,
+      True,
+      vis_func,
+      wgt_func,
+    )
+    np.save("radiomesh/tests/data/dirty.npy", dirty_dft)
+
+  dirty = vis2im_wgrid(
+    uvw,
+    freq,
+    vis,
+    wgt,
+    flag,
+    jones,
+    ant1,
+    ant2,
+    nx,
+    ny,
+    cell,
+    cell,
+    pol,
+    product,
+    ncorr,
+  )
+
+  # we compare fractional differences because abs values can be very large
+  diff = (dirty - dirty_dft) / dirty_dft.max()
+  assert np.allclose(1 + diff, 1, rtol=1e-4, atol=1e-4)
+
+
+# wgridder verbosity with
+# nthreads=1, dirty=(204x204), grid=(480x480x5), alpha=5, eps=0.0001
+# nrow=21060, nchan=8, nvis=168480/168480
+# w=[0.00377211; 667.09], min(n-1)=-7.63818e-05, dw=5564.15, (wmax-wmin)/dw=0.11989
+# memory overhead: 0.000373185GB (index) + 0.00171661GB (2D arrays)
+
+
+# sigma=2.3529411764705883,
+# alpha=6,
+# beta=2.1406712849934895,
+# mu=0.5284273783365884,
