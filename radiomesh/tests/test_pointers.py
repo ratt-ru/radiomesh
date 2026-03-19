e62c9d3dbe7a2c593ee24dbb329c10b2507b88d1
Simon Perkins <simon.perkins@gmail.com>
2026-03-19 14:09:53 +0200

    Update test cases

--- radiomesh/tests/test_pointers.py
+++ radiomesh/tests/test_pointers.py
@@ -6,6 +6,8 @@ from numba import types
 
 
 def test_pointers():
+  """Test atomic operations on arrays for integer primitives,
+  as well as integers in records"""
   RecordType = types.Record(
     [
       ("count1", {"type": types.uint64, "offset": 0}),
@@ -15,13 +17,18 @@ def test_pointers():
     aligned=True,
   )
 
-  @numba.njit
+  @numba.njit(nogil=True)
   def do_test():
-    counters = np.zeros((3, 3), dtype=RecordType)
     A = np.zeros((3, 3), dtype=np.uint64)
-    counters.item_ptr(1, 2).field_ptr("count1").atomic_inc()
-    assert counters[1, 2]["count1"] == 1
-    A.item_ptr(1, 1).atomic_inc()
+    assert A.item_ptr(1, 1).atomic_rmw("add", 1) == 0
     assert A[1, 1] == 1
+    assert A.item_ptr(1, 1).atomic_rmw("sub", 1) == 1
+    assert A[1, 1] == 0
+
+    counters = np.zeros((3, 3), dtype=RecordType)
+    assert counters.item_ptr(1, 2).field_ptr("count1").atomic_rmw("add", 1) == 0
+    assert counters[1, 2]["count1"] == 1
+    assert counters.item_ptr(1, 2).field_ptr("count1").atomic_rmw("sub", 1) == 1
+    assert counters[1, 2]["count2"] == 0
 
   do_test()
