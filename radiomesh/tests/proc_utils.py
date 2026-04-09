8cdef9ecb137fc87cc41a00dd70a5a4a76b9f5ec
Simon Perkins <simon.perkins@gmail.com>
2026-04-09 17:56:55 +0200

    Add a DatumLiteral caching test

---
+++ radiomesh/tests/proc_utils.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+
+def _init_numba_cache_debugging_with_capture(cache_dir, stdout_path, stderr_path):
+  assert "numba" not in sys.modules
+  os.environ["NUMBA_CACHE_DIR"] = cache_dir
+  os.environ["NUMBA_DEBUG_CACHE"] = "1"
+
+  sys.stdout = open(stdout_path, "a")
+  sys.stderr = open(stderr_path, "a")
